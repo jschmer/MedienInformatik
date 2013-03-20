26434c56dc1103e5b7c94550c81c05b0d615e190
Jens Schmer <der.liggers@gmail.com>
2013-03-20 19:48:53 +0100

    refactored main render loop into an "App" class

---
+++ MSVC/SampleSFML/include/SFMLApp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Event.h>
+
+#include <SFML/Graphics.hpp>
+
+class SFMLApp : public Event {
+public:
+    SFMLApp();
+    ~SFMLApp();
+
+    // render loop
+    int Run();
+
+private:
+    // helper
+    void RenderPixels();
+    void RenderScene();
+
+    // render loop events
+    bool OnInit();
+    void OnRender();
+    void OnCleanup();
+
+    // event handlers
+    void OnClosed() override;
+    void OnResized(uint width, uint height) override;
+
+private:
+    // instance data
+    bool _running;
+    sf::Window  _window;
+};
