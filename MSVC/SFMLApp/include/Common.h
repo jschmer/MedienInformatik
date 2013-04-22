47dab3491521d7c71f0f18f59f82b7f94296b26a
Jens Schmer <der.liggers@gmail.com>
2013-04-22 21:14:02 +0200

    added copyright notice and license

--- MSVC/SFMLApp/include/Common.h
+++ MSVC/SFMLApp/include/Common.h
@@ -1,3 +1,8 @@
+/* 
+ * Copyright (c) 2013 Jens Schmer
+ * This file is distributed under the MIT License.
+ * Consult COPYING within this package for further information. 
+ */
 #pragma once
 
 // Loading static libs via visual studio pragma extension for easier use of this lib.
