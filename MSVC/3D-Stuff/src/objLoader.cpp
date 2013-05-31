231896bcf24c6332c527ffa8d7d81ef2f4132c75
Jens Schmer <der.liggers@gmail.com>
2013-05-31 11:22:28 +0200

    more performance improvements

--- MSVC/3D-Stuff/src/objLoader.cpp
+++ MSVC/3D-Stuff/src/objLoader.cpp
@@ -11,47 +11,10 @@
 using namespace std;
 using glm::vec3;
 
-// Function to read input data values
-bool readvals(stringstream &s, const int numvals, GLfloat* values) {
-    for (int i = 0; i < numvals; i++) {
-        string value;
-        s >> value;
-
-        try {
-            auto v = std::stof(value);
-            values[i] = v;
-        }
-        catch (std::exception) {
-            cout << "Failed reading value " << i << " will skip\n";
-            return false;
-        }
-    }
-
-    return true;
-}
-
-bool readvals(stringstream &s, const int numvals, int* values) {
-    for (int i = 0; i < numvals; i++) {
-        string value;
-        s >> value;
-
-        try {
-            auto v = std::stoi(value);
-            values[i] = v;
-        }
-        catch (std::exception) {
-            cout << "Failed reading value " << i << " will skip\n";
-            return false;
-        }
-    }
-
-    return true;
-}
-
 //string split helper
 vector<string> split(string s, char delim) {
     vector<string> ret;
-    ret.reserve(s.length());
+    ret.reserve(10u);
 
     auto idx0 = 0u;
     auto idx1 = s.find(delim);
@@ -63,6 +26,57 @@ vector<string> split(string s, char delim) {
     ret.push_back(s.substr(idx0));
 
     return ret;
+}
+
+// Function to read input data values
+bool readvals(string &s, const unsigned int numvals, GLfloat* values) {
+
+    auto parts = split(s, ' ');
+
+    if (parts.size() > numvals) {
+        cout << "More values requested than available! will skip...\n";
+       return false;
+    }
+
+    for (auto i = 0u; i < numvals; i++) {
+        auto& str = parts[i];
+
+        try {
+            auto v = std::stof(str);
+            values[i] = v;
+        }
+        catch (std::exception) {
+            cout << "Failed reading value " << i << " will skip\n";
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool readvals(string &s, const unsigned int numvals, int* values) {
+
+    auto parts = split(s, ' ');
+
+    if (parts.size() > numvals) {
+        cout << "More values requested than available! will skip...\n";
+        return false;
+    }
+
+    for (auto i = 0u; i < numvals; i++) {
+        auto& str = parts[i];
+
+        try {
+            auto v = std::stoi(str);
+            values[i] = v;
+        }
+        catch (std::exception) {
+            cout << "Failed reading value " << i << " will skip\n";
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // helper to push float members of a vec3 to a vector
@@ -86,7 +100,7 @@ void addToVec(std::vector<const GLfloat> &c, const set<Face*> &s) {
 void loadObj(string obj_file, Obj &obj) {
     // disable syncing with c stdio functions, we only need c++ streams!
     // improves performance of streams a lot!
-    ifstream::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
 
     ifstream file(obj_file);
 
@@ -94,6 +108,7 @@ void loadObj(string obj_file, Obj &obj) {
         auto& faces    = obj.faces;
         auto& vertices = obj.vertices;
 
+        // count lines in file
         auto count = static_cast<unsigned int>(std::count(std::istreambuf_iterator<char>(file),
                                                           std::istreambuf_iterator<char>(),
                                                           '\n'));
@@ -102,6 +117,9 @@ void loadObj(string obj_file, Obj &obj) {
         file.clear();
         file.seekg(0, ios::beg);
 
+        // reserve enough space to make sure that the container won't ever resize!
+        // a resize would invalidate all references and pointer to its objects and i
+        // have these!
         faces.clear();
         faces.reserve(count);
         vertices.clear();
@@ -119,15 +137,11 @@ void loadObj(string obj_file, Obj &obj) {
             if (line.empty())
                 continue;
 
-            stringstream ssline(line);
-
             // extract mode (v, f, vn, ...)
-            string mode;
-            ssline >> mode;
+            auto mode = split(line, ' ')[0];
 
             // erase mode from input line
-            line = string(line.begin()+2, line.end());
-            ssline.str(line);
+            line = line.substr(mode.length()+1);
 
             // interpret mode
             if (mode == "#")
@@ -136,7 +150,7 @@ void loadObj(string obj_file, Obj &obj) {
             else if (mode == "v") {
                 // read vertex data
                 vector<float> coords(3, 0.0f);
-                if (readvals(ssline, 3, &coords[0])) {
+                if (readvals(line, 3, &coords[0])) {
                     vertices.push_back(vec3(coords[0], coords[1], coords[2]));
                 }
             }
@@ -145,18 +159,21 @@ void loadObj(string obj_file, Obj &obj) {
                 // read triangle face, vertices are indexed beginning from 1
 
                 // strip out any normals or texture specification
-                auto vertex_indices = split(line, ' ');
-                auto stripped_indices = accumulate(begin(vertex_indices),
-                                                   end(vertex_indices),
-                                                   string(),
-                                                   [](string &left, string right) -> string {
-                                                       auto spl = split(right, '/');
-                                                       return left + " " + spl[0];
-                                                   });
-                ssline.str(stripped_indices);
+                if (line.find("/") != string::npos) {
+                    auto vertex_indices = split(line, ' ');
+                    auto stripped_indices = accumulate(begin(vertex_indices),
+                                                       end(vertex_indices),
+                                                       string(),
+                                                       [](string &left, string right) -> string {
+                                                           auto spl = split(right, '/');
+                                                           return left + " " + spl[0];
+                                                    });
+                    line = stripped_indices.substr(1); // remove leading whitespace
+                }
+
                 vector<int> idx(3, 0);
-                if (readvals(ssline, 3, &idx[0])) {
-                    std::transform(begin(idx), end(idx), begin(idx), [](int i) { return --i; });
+                if (readvals(line, 3, &idx[0])) {
+                    std::transform(begin(idx), end(idx), begin(idx), [](int i) -> int { return i-1; });
 
                     auto& v1 = vertices[idx[0]];
                     auto& v2 = vertices[idx[1]];
@@ -169,8 +186,6 @@ void loadObj(string obj_file, Obj &obj) {
                     vtx_triangle_map[&v1].insert(&faces.back());
                     vtx_triangle_map[&v2].insert(&faces.back());
                     vtx_triangle_map[&v3].insert(&faces.back());
-
-                    face.normal;
                 }
             }
         }
@@ -181,8 +196,14 @@ void loadObj(string obj_file, Obj &obj) {
         auto& gl_vertices = obj.gl_vertices;
         auto& gl_normals  = obj.gl_normals;
         auto& gl_normals_average  = obj.gl_normals_average;
+
         gl_vertices.clear();
         gl_vertices.reserve(vertices.size());
+        gl_normals.clear();
+        gl_normals.reserve(vertices.size());
+        gl_normals_average.clear();
+        gl_normals_average.reserve(vertices.size());
+
         for (auto& face : faces) {
             addToVec(gl_vertices, face.v0);
             addToVec(gl_vertices, face.v1);
